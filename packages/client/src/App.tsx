41ef3260887785e4989db1db9058cbf16471b943
Belsea <44635556+Belsea@users.noreply.github.com>
2019-11-25 11:58:15 +0100

    Added items to Sidebar

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
-
-const GET_POKEMON = gql`
-    query {
-        pokemons(q: "Bulbasaur") {
-            edges {
-                node {
-                    id
-                    types
-                }
-            }
-        }
-    }
-`;
+import { Layout } from 'antd';
+import Sidebar from './components/Sidebar';
 
 export default function App() {
-    const { loading, error, data } = useQuery(GET_POKEMON);
-    if (loading) return <p>Loading ...</p>;
-    if (error) return <p>Error</p>;
     return (
-        <h1>
-            HENLO{' '}
-            {data.pokemons.edges.map((el: any) => (
-                <div>{el.node.id}</div>
-            ))}
-            !
-        </h1>
+        <Layout>
+            <Sidebar />
+        </Layout>
     );
 }
---
+++ packages/client/src/components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import { Layout, Input, Menu } from 'antd';
+import { string } from 'prop-types';
+
+const { Sider } = Layout;
+const { Search } = Input;
+
+const GET_POKEMON_TYPES = gql`
+    query {
+        pokemons(limit: 150) {
+            edges {
+                node {
+                    types
+                }
+            }
+        }
+    }
+`;
+
+export default function Sidebar(props: any) {
+    const { loading, error, data } = useQuery(GET_POKEMON_TYPES);
+    if (loading) return <p>Loading ...</p>;
+    if (error) return <p>Error</p>;
+    let types = [...new Set(data.pokemons.edges.flatMap((el: any) => el.node.types))].sort();
+    types.unshift('All');
+
+    return (
+        <Sider
+            breakpoint='lg'
+            collapsedWidth='0'
+            style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0
+            }}
+        >
+            <br />
+            <Search placeholder='Search by name' onSearch={(value) => console.log(value)} style={{ width: 200 }} />
+            <br />
+            <br />
+            <Menu theme='dark' mode='inline' defaultSelectedKeys={['0']}>
+                {types.map((type: any, index: number) => {
+                    return (
+                        <Menu.Item key={index}>
+                            <span className='nav-text'>{type}</span>
+                        </Menu.Item>
+                    );
+                })}
+            </Menu>
+        </Sider>
+    );
+}
